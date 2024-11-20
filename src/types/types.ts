bd1bc4fe4aff9b197117243f6f73ff14596d1a75
Sulaimon Taofik <Sulaimontaofik08@gmail.com>
2024-11-20 15:10:33 +0100

    Configured all for production

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,20 +1,20 @@
-
-  export interface Cast {
+export interface Cast {
     id: number;
     name: string;
     character: string;
   }
+  
   export interface Genre {
     id: number;
     name: string;
   }
+  
   export interface Movie {
     id: number;
     title: string;
     poster_path: string;
     release_date: string;
     vote_average: number;
-    overview?: string; // Make overview optional if not always present
-    genres?: any[]; // Optional if not always used
+    overview?: string;
+    genres?: Genre[]; // Use the Genre interface instead of any[]
   }
-  
